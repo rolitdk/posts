501fc4e15312a2518145fb6636f80b9400d7ebc6
Svetlana Gorelova <v35.ma22@gmail.com>
2026-04-27 17:36:07 +0300

    Integrate redux into app

--- src/components/Link/index.jsx
+++ src/components/Link/index.jsx
@@ -1,9 +1,5 @@
 import * as SC from "./styles";
 
-export const Link = ({ simple = true, children, ...props }) => {
-  return simple ? (
-    <SC.SimpleLink {...props}>{children}</SC.SimpleLink>
-  ) : (
-    <SC.NavigationLink {...props}>{children}</SC.NavigationLink>
-  );
-};
+export const Link = ({ children, ...props }) => (
+  <SC.SimpleLink {...props}>{children}</SC.SimpleLink>
+);
