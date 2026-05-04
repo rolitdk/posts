ba5793084cd41ab1f4d7c283d3a6cef1718b9443
Svetlana Gorelova <v35.ma22@gmail.com>
2026-05-04 13:10:38 +0300

    add edit post

--- src/pages/posts/edit/index.jsx
+++ src/pages/posts/edit/index.jsx
@@ -1,1 +1,29 @@
-export const EditPostPage = () => <>Это страничка редактирования поста</>
+import { useParams } from "react-router-dom";
+import { PostForm } from "../components/PostForm";
+import { useSelector, useDispatch } from "react-redux";
+import { editPost } from "../../../redux/slices/postsSlice";
+
+export const EditPostPage = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  const { list } = useSelector((state) => state.posts.posts);
+
+  const onSubmitForm = (formValues) => {
+    dispatch(editPost(formValues));
+  };
+
+  if (!list) {
+    return <>Пост не найден</>;
+  }
+
+  const findedPost = list.find((item) => item.id === Number(id));
+
+  return (
+    <PostForm
+      title={`Редактирование поста - ${id}`}
+      onSubmitForm={onSubmitForm}
+      defaultValues={findedPost}
+    />
+  );
+};
